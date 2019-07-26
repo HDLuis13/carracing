0f80d2303c3250fc66f8a62cb4157db013dfbb3a
HDLuis13 <Luis.Habenicht@gmail.com>
2019-07-26 11:51:41 +0200

    Changed function

--- PPO2/ppo2_carracing.py
+++ PPO2/ppo2_carracing.py
@@ -3,32 +3,52 @@ from gym.envs.box2d import CarRacing3
 from stable_baselines.common.policies import CnnPolicy
 from stable_baselines import PPO2
 from stable_baselines.common.vec_env.dummy_vec_env import DummyVecEnv
+from stable_baselines.bench import Monitor
+from stable_baselines.gail import generate_expert_traj
+import os
+import cv2
+import csv
+import gym
 
 # grayscale=True, vae_input=False, skip_frames=3, num_stack=4, continuous=True, render_on_learn=False
-env = CarRacing3(
-)
+log_dir = "/tmp/gym/"
+os.makedirs(log_dir, exist_ok=True)
+
+env = gym.make('CarRacing-v0')
+env = Monitor(env, log_dir, allow_early_resets=True)
 env = DummyVecEnv([lambda: env])
 
-model = PPO2(CnnPolicy, env=env, verbose=2, tensorboard_log='./t_log/', n_steps=2048, learning_rate=3e-4)
-model = PPO2.load("./t_log/ppo2_sf3_ns4_steps2048_100000.pkl", env=env, verbose=2, tensorboard_log='./t_log/')
-timesteps = 100000
-for i in range(9):
-    timesteps_total = timesteps*(i+1)+100000
-    model.learn(total_timesteps=timesteps, tb_log_name='ppo2_sf3_ns4_steps2048_{}'.format(timesteps_total), reset_num_timesteps=False)
-    model.save("./t_log/ppo2_sf3_ns4_steps2048_{}".format(timesteps_total))
+# model = PPO2(CnnPolicy, env=env, verbose=2, n_steps=2048, learning_rate=3e-4)
+# timesteps = 100000
+# for i in range(10):
+#     timesteps_total = timesteps*(i+1)
+#     model.learn(total_timesteps=timesteps,reset_num_timesteps=False)
+#     model.save("./t_log/ppo2_sf3_ns4_steps2048_{}".format(timesteps_total))
 
 
 
-# # Enjoy trained agent
-#
-# model = PPO2.load("./t_log/ppo2_compare_stable_1000000_v2.pkl", env=env, verbose=2)
-# # model.learn(total_timesteps=1000000, tb_log_name="testing")
-#
-#
-# obs = env.reset()
-#
-# while True:
-#     action, _states = model.predict(obs)
-#     obs, rewards, dones, info = env.step(action)
-#     env.render()
-# env.close()
+# Enjoy trained agent
+
+model = PPO2.load("./t_log/ppo2_compare_stable_500000.pkl", env=env, verbose=2)
+# model.learn(total_timesteps=1000000, tb_log_name="testing")
+
+
+obs = env.reset()
+print(obs.shape)
+value_list = [['timestep', 'value', 'action']]
+
+for update in range(1000):
+    action, _states, values = model.predict(obs)
+    value_list.append([update, values[0], action])
+    if 1000 > update > 0 == update % 50:
+        image_path = os.path.join('./images', "{}.png".format(update))
+        img = cv2.cvtColor(obs[0], cv2.COLOR_RGB2BGR)
+        cv2.imwrite(image_path, img)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+env.close()
+
+with open('./images/values.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(value_list)
+csvFile.close()
--- PPO2/ppo2_carracing_play.py
+++ PPO2/ppo2_carracing_play.py
@@ -1,19 +1,27 @@
-from gym.envs.box2d import CarRacing3
+from gym.envs.box2d.car_racing_3 import CarRacing3
 
 from stable_baselines.common.policies import CnnPolicy
 from stable_baselines import PPO2
 from stable_baselines.common.vec_env.dummy_vec_env import DummyVecEnv
+from stable_baselines.bench import Monitor
+import gym
+import os
 
+log_dir = "/tmp/gym/"
+os.makedirs(log_dir, exist_ok=True)
+
+env = gym.make('CarRacing-v0')
+env = Monitor(env, log_dir, allow_early_resets=True)
 # grayscale=True, vae_input=False, skip_frames=3, num_stack=4, continuous=True, render_on_learn=False
-env = CarRacing3(
-)
+# env = CarRacing3(num_stack=1, skip_frames=1, grayscale=True)
+#
 env = DummyVecEnv([lambda: env])
 
 # Enjoy trained agent
 
-model = PPO2.load("./t_log/ppo2_sf3_ns4_steps2048_200000", env=env, verbose=2)
+model = PPO2.load("./t_log/ppo2_compare_stable_500000", env=env, verbose=2)
 # model.learn(total_timesteps=1000000, tb_log_name="testing")
-
+print(model.observation_space)
 
 obs = env.reset()
 
